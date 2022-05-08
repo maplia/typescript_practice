d0f14f3ffd80f3a04d3f7c514917ad5920b32a66
Maple Leaves <sie@maplia.jp>
2022-05-08 14:29:16 +0900

    2.2 変数の宣言と使用

--- src/chapter2/sample2.1.ts
+++ src/chapter2/sample2.1.ts
@@ -1,9 +1,12 @@
-const greeting = "Hello, ";
-const text = greeting + "world!";
-// "Hello, world!"と表示される
-console.log(text);
-
-const i = 3;
-if (i < 10) {
-    console.log("iは10未満です");
+{
+    const greeting = "Hello, ";
+    const text = greeting + "world!";
+    // "Hello, world!"と表示される
+    console.log(text);
+}
+{
+    const i = 3;
+    if (i < 10) {
+        console.log("iは10未満です");
+    }
 }
---
+++ src/chapter2/sample2.2.ts
@@ -0,0 +1,10 @@
+{
+    const greeting: string = "Hello, ";
+    const target: string = "world!";
+    console.log(greeting + target);
+}
+{
+    let greeting = "Hello, ";
+    greeting = greeting + "world!";
+    console.log(greeting);
+}
